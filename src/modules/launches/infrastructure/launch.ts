c1631a1432f33c650c460952b5c2bac8e7828017
baptistedri <baptiste.drillien@gmail.com>
2021-10-25 21:11:34 +0200

    wip

--- src/modules/launches/infrastructure/launch.ts
+++ src/modules/launches/infrastructure/launch.ts
@@ -1,1 +1,10 @@
-export interface Launch {}
+export interface Launch {
+	name: string
+	slug: string
+	status: string
+}
+
+export interface LaunchStatus {
+	id: number
+	name: string
+}
--- src/modules/launches/infrastructure/launches.api.ts
+++ src/modules/launches/infrastructure/launches.api.ts
@@ -1,1 +1,8 @@
 import { api } from "@/config/axios-instance"
+import { LaunchesOutput } from "@/modules/launches/domain/launches.output"
+
+export class LaunchesApi implements LaunchesOutput {
+	getLaunches() {
+		return api.get("")
+	}
+}
