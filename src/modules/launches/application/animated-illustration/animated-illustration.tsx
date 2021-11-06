aa4bb2d596a446354ee12c790244da903128a118
baptistedri <baptiste.drillien@gmail.com>
2021-11-06 19:15:07 +0100

    added lottie animation

--- src/modules/launches/application/animated-illustration/animated-illustration.tsx
+++ src/modules/launches/application/animated-illustration/animated-illustration.tsx
@@ -1,9 +1,17 @@
 import styles from "./animated-illustration.module.scss"
+import { Player } from "@lottiefiles/react-lottie-player"
 
 export const AnimatedIllustration = () => {
 	return (
 		<div className={styles.wrapper}>
-			<div className={styles.container}>Illustration</div>
+			<div className={styles.container}>
+				<Player
+					autoplay
+					loop
+					speed={0.5}
+					src={"/animation/astronaut_lottie_animation.json"}
+				/>
+			</div>
 		</div>
 	)
 }
