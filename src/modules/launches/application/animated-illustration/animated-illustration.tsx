251bba3b8f98d574ef483dee49334b3c45ee6725
baptistedri <baptiste.drillien@gmail.com>
2021-11-06 18:04:22 +0100

    better radius

--- src/modules/launches/application/animated-illustration/animated-illustration.tsx
+++ src/modules/launches/application/animated-illustration/animated-illustration.tsx
@@ -3,7 +3,7 @@ import styles from "./animated-illustration.module.scss"
 export const AnimatedIllustration = () => {
 	return (
 		<div className={styles.wrapper}>
-			<div className={styles.container}>qsdqmsldkqlsmdksqlmkd</div>
+			<div className={styles.container}>Illustration</div>
 		</div>
 	)
 }
